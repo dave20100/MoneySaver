a7183df42031e1e9e7a89bae55bc68391ddce52e
Dawid <dave201000@gmail.com>
2019-12-03 13:52:37 +0100

    changed payment history to be part of wallet

--- app/src/main/java/com/example/moneysaver/Wallet.java
+++ app/src/main/java/com/example/moneysaver/Wallet.java
@@ -2,13 +2,20 @@ package com.example.moneysaver;
 
 public class Wallet {
     public int MoneyAmount;
+    public PaymentHistory PaymentHistory;
 
     public Wallet(int moneyAmount) {
         MoneyAmount = moneyAmount;
+        PaymentHistory = new PaymentHistory();
+    }
+
+    public com.example.moneysaver.PaymentHistory getPaymentHistory() {
+        return PaymentHistory;
     }
 
     public Wallet() {
         MoneyAmount = 0;
+        PaymentHistory = new PaymentHistory();
     }
 
     public int getMoneyAmount() {
