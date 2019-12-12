3660c9f87447ed15dd913b33a6067e79d45ff894
Dawid <dave201000@gmail.com>
2019-12-12 22:25:17 +0100

    fixed 2 warnings

--- app/src/main/java/com/example/moneysaver/PaymentDatabase.java
+++ app/src/main/java/com/example/moneysaver/PaymentDatabase.java
@@ -3,7 +3,7 @@ package com.example.moneysaver;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {PaymentHistory.class}, version = 1)
+@Database(entities = {PaymentHistory.class}, version = 1, exportSchema = false)
 public abstract class PaymentDatabase extends RoomDatabase {
     public abstract PaymentDao paymentDao();
 }
--- app/src/main/java/com/example/moneysaver/SalaryDatabase.java
+++ app/src/main/java/com/example/moneysaver/SalaryDatabase.java
@@ -3,7 +3,7 @@ package com.example.moneysaver;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {SalaryHistory.class}, version = 1)
+@Database(entities = {SalaryHistory.class}, version = 1, exportSchema = false)
 public abstract class SalaryDatabase extends RoomDatabase {
     public abstract SalaryDao salaryDao();
 }
